fe11af76624bf7a44495bf90e251db06d7d431c8
Marco Farruggio <marcofarruggiopersonal@gmail.com>
2024-12-21 10:03:02 +0000

    Update Training.py
    Optimized code
    Added customization for alert frequency
    Changed some internal names
    Removed redundant imports
    Added ._Typing for better pb typing

--- pybernetics/Training.py
+++ pybernetics/Training.py
@@ -1,50 +1,49 @@
-from typing import Tuple, List, Union
-import numpy as np
+from typing import List
 from . import Layers
-from . import Optimizers
 from . import __version__
+from ._Typing import Optimizer, LossFunction, Layer, Dataset
 
 class Loop:
     def __init__(
             self,
-            optimizer: Optimizers,
-            dataset: Tuple[np.ndarray, np.ndarray],
+            optimizer: Optimizer,
+            dataset: Dataset,
             loss_function,
-            layers: List[Union['Layers.Dense', 'Layers.ActivationFunction']],
+            layers: List[Layer],
             epochs: int = 1000,
-            announcements: bool = True,
-            show_output: bool = True
+            alert: bool = True,
+            alert_freq: int = 100,
+            debug: bool = False
         ) -> None:
 
+        # Initialize the training loop
         self.optimizer = optimizer
         self.loss_function = loss_function
         self.layers = layers
-        self.X, self.y = dataset  # Unpack the dataset into X and y (Paired dataset)
+        self.X, self.y = dataset  # Unpack the dataset into X and y (paired dataset)
+        self.epochs = epochs
+        self.alert = alert
+        self.alert_freq = alert_freq
+        self.debug = debug
 
-        bar_length = 16  # Length of the loading bar
-        first_iteration = True
-
-        layer_descriptions = []
-        for layer in layers:
-            if isinstance(layer, Layers.Dense): layer_descriptions.append("Dense")
-            elif isinstance(layer, Layers.ActivationFunction): layer_descriptions.append(layer.function.capitalize())
-            else: layer_descriptions.append("Unkown")
-
-        formatted_layers = " -> ".join(layer_descriptions)
+        # Progress bar settings
+        self.pbar_len = 10
+        self.first_loss = None
+        self.pbar_full_char = "#"
+        self.pbar_empty_char = "-"
 
         for epoch in range(1, epochs + 1):
-            # Perform a forward pass
             inputs = self.X
-
+            
+            # Perform a forward pass through the full network
             for layer in layers:
                 layer.forward(inputs)
                 inputs = layer.output
 
             # Compute loss
             loss = loss_function.compute(self.layers[-1].output, self.y)
-            if first_iteration is True:
-                first_loss = loss
-                first_iteration = False
+            if self.first_loss is None:
+                self.first_loss = loss
 
             # Perform a backward pass
             loss_function.backward(self.layers[-1].output, self.y)  # Start from the last layer's output
@@ -56,32 +55,31 @@ class Loop:
                 if isinstance(layer, Layers.Dense):
                     self.optimizer.update_params(layer)
 
-            if announcements:
-                # Update loading bar and print the loss
-                percent = (epoch) / epochs
-                num_hashes = int(bar_length * percent)
-                loading_bar = "#" * num_hashes + "-" * (bar_length - num_hashes)
+            # Calculate the percentage of completion
+            percent = (epoch / epochs) * 100
 
-                # Variables formatted nicely for printing
-                formatted_epochs = f"{epoch}/{epochs}"
-                formatted_loading_bar = f"[{loading_bar}]"
-                formatted_percentage = f"{(epoch / epochs) * 100:0<6.2f}%"[:7]
-                formatted_loss = f"{loss:.29f}"[:29]
-                formatted_total_improvement = f"{(first_loss - loss):0<16.16f}"[:16]
+            # Update loading bar and print the loss
+            num_full_chars = int(self.pbar_len * (percent / 100)) # Float - int conversion (via truncation)
+            pbar = f"[{self.pbar_full_char * num_full_chars}{self.pbar_empty_char * (self.pbar_len - num_full_chars)}]"
 
-                # Print the progress sheet
-                print(f"""===================================
-        Pybernetics v{__version__}        
-        Marco Farruggio
-===================================
-Training Loop Progress:
------------------------------------
-Layers: {formatted_layers}
-Epoch: {formatted_epochs}
-Loading: {formatted_loading_bar} {formatted_percentage}
-Loss: {formatted_loss}
-Total Improvement: {formatted_total_improvement}
-===================================""")
+            # Variables formatted nicely for printing
+            formatted_epochs = f"{epoch}/{epochs}"
+            formatted_percentage = f"{(epoch / epochs) * 100:0<6.2f}%"[:7]
+            formatted_loss = f"{loss:.5f}"[:7]
+            formatted_total_improvement = f"{(self.first_loss - loss):0<16.5f}"[:7]
+            
+            # If alerts are enabled, print the progress sheet
+            if self.alert:
+                if self.alert_freq is not None:
+                    # If the alert frequency is set, only print the progress sheet at those intervals
+                    if epoch % self.alert_freq == 0 or epoch == self.epochs:
+                        # Print the progress sheet
+                        print(f"Training: {pbar} {formatted_percentage} | Loss: {formatted_loss} | Total Improvement: {formatted_total_improvement} | Epochs: {formatted_epochs}")
+
+                # If there is no alert frequency, assume they want all alerts
+                else:
+                    # Print the progress sheet
+                    print(f"Training: {pbar} {formatted_percentage} | Loss: {formatted_loss} | Total Improvement: {formatted_total_improvement} | Epochs: {formatted_epochs}")
                 
-                if show_output:
-                    print(inputs)
+            if self.debug:
+                print(inputs) # Outputs names as 'inputs' due to feedforward loop's nature
