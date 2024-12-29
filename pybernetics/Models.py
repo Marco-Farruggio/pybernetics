6d6c25b903e23722cb887e15c32dfe0c162a4f07
Marco Farruggio <marcofarruggiopersonal@gmail.com>
2024-12-29 20:18:07 +0000

    Update Models.py
    Added way more functionality and clean up some stuff

--- pybernetics/Models.py
+++ pybernetics/Models.py
@@ -29,9 +29,74 @@ Parent model for all neural network typical architectre models, such as:
 """
 
 from . import Training
+from ._Utils import Helpers
 from ._Typing import Layer, Optimizer, LossFunction, Dataset
+import numpy as np
+
+DeprecatedWarning = Helpers.DeprecatedWarning
+NotYetImplementedWarning = Helpers.NotYetImplementedWarning
 
 class Sequential:
+    """
+    Sequential
+    ==========
+
+    - A feedforward neural network model.
+    - Contains a list of layers, an optimizer, and a loss function.
+    - Can be used to train a neural network on a dataset.
+    - Uses n(um)p(y).ndarray(s) for all data processing.
+
+    Methods:
+    --------
+    'add':
+        - Add a layer to the model.
+
+    'fit'/'train':
+        - Train the model on a dataset.
+
+    'save':
+        - Save the model to a file.
+        - Not yet implemented.
+    
+    'load':
+        - Load the model from a file.
+        - Not yet implemented.
+
+    'process':
+        - Process an input through the model.
+        - Does not train the model.
+
+    Parameters:
+    -----------
+    'layers': List[Layer]
+        - The layers of the model.
+
+    'optimizer': Optimizer
+        - The optimizer to use when updating the weights of the model.
+    
+    'loss_function': LossFunction
+        - The loss function to use when computing the loss of the model.
+    
+    Example:
+    --------
+    ```
+    import pybernetics as pb
+
+    sgd_optimizer = pb.Optimizers.SGD(0.01)
+    cc_loss = pb.Loss.CC()
+    sd_dataset = pb.Datasets.spiral_data(100, 3)
+
+    pbnn = pb.Models.Sequential([
+        pb.Layers.Dense(2, 3, "random"),
+        pb.Layers.ActivationFunction("sigmoid"),
+        pb.Layers.Dense(3, 3, "random"),
+        pb.Layers.ActivationFunction("softmax")],
+        optimizer = sgd_optimizer,
+        loss_function = cc_loss)
+
+    pbnn.fit(sd_dataset, 1000)
+    ```
+    """
     def __init__(
             self,
             layers: Layer = None,
@@ -59,7 +124,7 @@ class Sequential:
             dataset: Dataset = None,
             epochs: int = 1000,
             alert: bool = True,
-            alert_freq: int = 100,
+            alert_freq: int = None,
             debug: bool = False
         ) -> None:
         
@@ -75,6 +140,55 @@ class Sequential:
 
     def train(self, *args, **kwargs) -> None:
         self.fit(*args, **kwargs)
+
+    def save(self, filename: str = "PyberneticsSequentialModel.csv") -> None:
+        """
+        Save the model to a file.
+        Puts the model into a non volatile storage.
+        New model is usually in a non readable format.
+        Support for multiple file extension to be implemented.
+        """
+        NotYetImplementedWarning("Saving a model is not yet implemented.")
+
+    def load(self, filename: str = "PyberneticsSequentialModel.csv") -> None:
+        NotYetImplementedWarning("Loading a model is not yet implemented.")
+
+    def __repr__(self) -> str:
+        return f"<Sequential Model: {len(self._model_layers)} layers>"
+
+    def __str__(self) -> str:
+        return f"Sequential Model: {len(self._model_layers)} layers"
+
+    def __len__(self) -> int:
+        return len(self._model_layers)
+
+    def __getitem__(self, index: int) -> Layer:
+        return self._model_layers[index]
+
+    def __setitem__(self, index: int, layer: Layer) -> None:
+        self._model_layers[index] = layer
+
+    def __delitem__(self, index: int) -> None:
+        del self._model_layers[index]
+
+    def __iter__(self):
+        return iter(self._model_layers)
+    
+    def __reversed__(self):
+        return reversed(self._model_layers)
+    
+    def __contains__(self, layer: Layer) -> bool:
+        return layer in self._model_layers
+
+    def process(self, input: np.ndarray) -> np.ndarray:
+        for layer in self._model_layers:
+            layer.forward(input)
+            input = layer.output
+        
+        return input
+    
+    def __call__(self, input: np.ndarray) -> np.ndarray:
+        return self.process(input)
 
 # NOTE: Ways to save a model coming soon,
 # with the 'save' method, and 'load' method.
