aa5240b5b07448c1a5ee629da53fbc4d53c21968
Marco Farruggio <marcofarruggiopersonal@gmail.com>
2025-01-01 10:04:17 +0000

    Update testing.py

--- testing.py
+++ testing.py
@@ -7,24 +7,24 @@ sd_dataset = pb.Datasets.spiral_data(100, 3)
 
 pbnn = pb.Models.Sequential([
     pb.Layers.Dense(2, 3, "random"),
-    pb.Layers.ActivationFunction("sigmoid"),
+    pb.Layers.Sigmoid(-750, 750),
+    pb.Layers.Dense(3, 3, "random"),
+    pb.Layers.Tanh(),
     pb.Layers.Dense(3, 3, "random")],
     optimizer = sgd_optimizer,
     loss_function = cc_loss)
 
-pbnn.fit(sd_dataset, 1000, debug=True)
+pbnn.fit(sd_dataset, 1000)
 
 inputs = np.array([[0.1, 0.2]])
 print(pbnn.process(inputs))
 
-# Example usage:
 my_array = pb.PyArrays.PyArray((2, 3, 4), fill=0)
 print(my_array)
 
 my_array2 = pb.PyArrays.PyArray((3, 2), fill=0)
 print(my_array2)
 
-# Create an identity-like array
 identity_matrix = pb.PyArrays.identity((3, 3))
 print(identity_matrix)
 
